418097352329aa18893a03d6ebf13101bca8d73b
Mayur:) <mayur.das@ralali.com>
2020-01-08 15:45:35 +0700

    interface testcase fixe

--- setup_koans_test.go
+++ setup_koans_test.go
@@ -35,7 +35,7 @@ func TestKoans(t *testing.T) {
 	aboutVariadicFunctions()
 	aboutDefer()
 	aboutFiles()
-	// aboutInterfaces()
+	aboutInterfaces()
 	// aboutCommonInterfaces()
 	// aboutMaps()
 	// aboutPointers()
